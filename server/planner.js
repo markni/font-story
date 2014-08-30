8167018d1d0ca7fd4ed0677dade5096fc550d341
Mark Ni <xixian.ni@gmail.com>
2014-08-30 04:26:17 -0700

    allow both development and production to grab data

--- server/planner.js
+++ server/planner.js
@@ -1,16 +1,14 @@
 var schedule = require('node-schedule');
 var Service = require('./service');
 
-
 var rule = new schedule.RecurrenceRule();
-rule.minute = [0,7,15,25,30,40,45,50,55];
+rule.minute = [0, 7, 15, 25, 30, 40, 45, 50, 55];
 
-  if(process.env.NODE_ENV.toLowerCase() !== 'development')  {
-		var j = schedule.scheduleJob(rule, function(){
-			Service.getFontsFromPublicEvents();
+var j = schedule.scheduleJob(rule, function () {
+	Service.getFontsFromPublicEvents();
 
-		});
-
+});
 
 
-	}
+
+
